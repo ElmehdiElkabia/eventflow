f6bd2def0cfee0e4f9522a47a323a5ff882da23c
elmehdi elkabia <elmehdielkabia@gmail.com>
2025-06-06 12:43:50 +0100

    create event_schedules table

---
+++ backend/database/migrations/2025_06_06_114306_create_event_schedules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->string('time');
+            $table->string('title');
+            $table->string('type'); // keynote, panel, break, workshop, etc.
+            $table->string('speaker')->nullable();
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_schedules');
+    }
+};
